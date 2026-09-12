d8248861b3d5f2dd7500243b4a0cd2622471f850
kkido <kaoru.kido@icloud.com>
2026-09-12 19:27:22 +0900

    main修正

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kkido <kkido@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 12:51:06 by kkido             #+#    #+#             */
-/*   Updated: 2026/09/12 18:24:22 by kkido            ###   ########.fr       */
+/*   Updated: 2026/09/12 19:26:06 by kkido            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,31 @@
 #include "Bureaucrat.hpp"
 
 int main() {
-  Bureaucrat Alice = Bureaucrat(90, "Alice");
-  Bureaucrat Top = Bureaucrat(1, "Top");
-  Bureaucrat Bottom = Bureaucrat(150, "Bottom");
+  std::cout << "--- default constructer test ---" << std::endl;
+  Bureaucrat Alice(90, "Alice");
+  std::cout << Alice << std::endl;
+  try {
+    Bureaucrat Bob(0, "Bob");
+  } catch (const Bureaucrat::GradeTooHighException&) {
+    std::cout << "catch GradeTooHighException." << std::endl;
+  }
+  try {
+    Bureaucrat Bob(0, "Bob");
+  } catch (const std::exception& e) {
+    std::cout << "catch std::exception& e." << std::endl;
+  }
+  try {
+    Bureaucrat Bob(151, "Bob");
+  } catch (const Bureaucrat::GradeTooLowException&) {
+    std::cout << "catch GradeLowHighException." << std::endl;
+  }
+  try {
+    Bureaucrat Bob(151, "Bob");
+  } catch (const std::exception& e) {
+    std::cout << "catch std::exception& e." << std::endl;
+  }
+  Bureaucrat Top(1, "Top");
+  Bureaucrat Bottom(150, "Bottom");
   std::cout << "--- standard increment decrement test ---" << std::endl;
   std::cout << Alice << std::endl;
   std::cout << "--- incrementGrade() ---" << std::endl;
@@ -31,8 +53,8 @@ int main() {
   std::cout << "--- incrementGrade() ---" << std::endl;
   try {
     Top.incrementGrade();
-  } catch (const Bureaucrat::GradeTooHighException) {
-    std::cout << "increment failed." << std::endl;
+  } catch (const Bureaucrat::GradeTooHighException&) {
+    std::cout << "catch GradeTooHighException." << std::endl;
   }
   std::cout << Top << std::endl;
   std::cout << "--- too low grade test ---" << std::endl;
@@ -40,8 +62,8 @@ int main() {
   std::cout << "--- decrementGrade() ---" << std::endl;
   try {
     Bottom.decrementGrade();
-  } catch (const Bureaucrat::GradeTooLowException) {
-    std::cout << "decrement failed." << std::endl;
+  } catch (const Bureaucrat::GradeTooLowException&) {
+    std::cout << "catch GradeTooLowException." << std::endl;
   }
   std::cout << Bottom << std::endl;
   return 0;
