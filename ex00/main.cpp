3be9cca981a782a9813cdddae588f614564a1b3a
kkido <kaoru.kido@icloud.com>
2026-09-13 21:52:16 +0900

    typoを修正

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: kkido <kkido@student.42tokyo.jp>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 12:51:06 by kkido             #+#    #+#             */
-/*   Updated: 2026/09/12 19:26:06 by kkido            ###   ########.fr       */
+/*   Updated: 2026/09/13 17:29:02 by kkido            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 #include "Bureaucrat.hpp"
 
 int main() {
-  std::cout << "--- default constructer test ---" << std::endl;
+  std::cout << "=== default constructor test ===" << std::endl;
   Bureaucrat Alice("Alice", 90);
   std::cout << Alice << std::endl;
   try {
@@ -31,7 +31,7 @@ int main() {
   try {
     Bureaucrat Bob("Bob", 151);
   } catch (const Bureaucrat::GradeTooLowException&) {
-    std::cout << "catch GradeLowHighException." << std::endl;
+    std::cout << "catch GradeTooLowException." << std::endl;
   }
   try {
     Bureaucrat Bob("Bob", 151);
@@ -40,26 +40,26 @@ int main() {
   }
   Bureaucrat Top("Top", 1);
   Bureaucrat Bottom("Bottom", 150);
-  std::cout << "--- standard increment decrement test ---" << std::endl;
+  std::cout << "=== standard increment decrement test ===" << std::endl;
   std::cout << Alice << std::endl;
-  std::cout << "--- incrementGrade() ---" << std::endl;
+  std::cout << "=== incrementGrade() ===" << std::endl;
   Alice.incrementGrade();
   std::cout << Alice << std::endl;
-  std::cout << "--- decrementGrade() ---" << std::endl;
+  std::cout << "=== decrementGrade() ===" << std::endl;
   Alice.decrementGrade();
   std::cout << Alice << std::endl;
-  std::cout << "--- too high grade test ---" << std::endl;
+  std::cout << "=== too high grade test ===" << std::endl;
   std::cout << Top << std::endl;
-  std::cout << "--- incrementGrade() ---" << std::endl;
+  std::cout << "=== incrementGrade() ===" << std::endl;
   try {
     Top.incrementGrade();
   } catch (const Bureaucrat::GradeTooHighException&) {
     std::cout << "catch GradeTooHighException." << std::endl;
   }
   std::cout << Top << std::endl;
-  std::cout << "--- too low grade test ---" << std::endl;
+  std::cout << "=== too low grade test ===" << std::endl;
   std::cout << Bottom << std::endl;
-  std::cout << "--- decrementGrade() ---" << std::endl;
+  std::cout << "=== decrementGrade() ===" << std::endl;
   try {
     Bottom.decrementGrade();
   } catch (const Bureaucrat::GradeTooLowException&) {
